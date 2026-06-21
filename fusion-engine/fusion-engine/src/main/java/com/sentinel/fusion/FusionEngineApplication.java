132baf1d9d742d59cb5ea65160a93649f3358c73
Nhutnguyen395 <nhutnguyen395@gmail.com>
2026-06-20 17:29:06 -0700

    created fusion-engine spring initilizer and setup

---
+++ fusion-engine/fusion-engine/src/main/java/com/sentinel/fusion/FusionEngineApplication.java
@@ -0,0 +1,13 @@
+package com.sentinel.fusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusionEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FusionEngineApplication.class, args);
+    }
+
+}
---
+++ fusion-engine/fusion-engine/src/test/java/com/sentinel/fusion/FusionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentinel.fusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FusionEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
